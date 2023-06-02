fe653f085c219a25da059ed0650d49e38b0ec49d
Guillaume <guillaume.cirillo@gmail.com>
2023-06-02 14:00:25 +0200

    feat: 🎸 Menu

---
+++ Shotass/Shotass_menu_BackUpThisFolder_ButDontShipItWithYourGame/il2cppOutput/Assembly-CSharp_CodeGen.c
@@ -0,0 +1,61 @@
+﻿#include "pch-c.h"
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+// 0x00000001 System.Void ty::Start()
+extern void ty_Start_mB7A559CCF538F759833EDE67F45EC0EB6C24C9F5 (void);
+// 0x00000002 System.Void ty::Update()
+extern void ty_Update_mAAD962554F3407E3EB7D8B4837D548DE9843BF04 (void);
+// 0x00000003 System.Void ty::.ctor()
+extern void ty__ctor_mCA78CB719B56E83B6CF220F563D0FFC39C3BDEBF (void);
+// 0x00000004 System.Void Unity.Template.VR.XRPlatformControllerSetup::Start()
+extern void XRPlatformControllerSetup_Start_m1F22FCA29DFD83DC0E343C3F391D04A7C52085BF (void);
+// 0x00000005 System.Void Unity.Template.VR.XRPlatformControllerSetup::.ctor()
+extern void XRPlatformControllerSetup__ctor_mF9A3998AF90962CF8F35BAF2221558BDF5F6596E (void);
+static Il2CppMethodPointer s_methodPointers[5] = 
+{
+	ty_Start_mB7A559CCF538F759833EDE67F45EC0EB6C24C9F5,
+	ty_Update_mAAD962554F3407E3EB7D8B4837D548DE9843BF04,
+	ty__ctor_mCA78CB719B56E83B6CF220F563D0FFC39C3BDEBF,
+	XRPlatformControllerSetup_Start_m1F22FCA29DFD83DC0E343C3F391D04A7C52085BF,
+	XRPlatformControllerSetup__ctor_mF9A3998AF90962CF8F35BAF2221558BDF5F6596E,
+};
+static const int32_t s_InvokerIndices[5] = 
+{
+	5277,
+	5277,
+	5277,
+	5277,
+	5277,
+};
+IL2CPP_EXTERN_C const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule;
+const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule = 
+{
+	"Assembly-CSharp.dll",
+	5,
+	s_methodPointers,
+	0,
+	NULL,
+	s_InvokerIndices,
+	0,
+	NULL,
+	0,
+	NULL,
+	0,
+	NULL,
+	NULL,
+	NULL, // module initializer,
+	NULL,
+	NULL,
+	NULL,
+};
